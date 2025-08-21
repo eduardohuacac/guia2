cbe8e50856bd28a0425327ed0d10f3343f3b4600
Eduardo Huacac <eduardoahr2324@gmail.com>
2025-08-21 17:40:27 -0500

    ejemplo 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:guia2/screens/home_screen.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Libro de plantas",
-      home: PlantsBook(),
-    );
-  }
+void main() {
+  runApp(const MainApp());
 }
 
-class PlantsBook extends StatelessWidget {
-  const PlantsBook({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber.shade300,
-          title: Text(
-            'Medicina Natural',
-            style: TextStyle(color: const Color.fromARGB(255, 8, 8, 8)),
-          ),
-          bottom: TabBar(
-            indicatorColor: Colors.blue,
-            labelColor: Colors.white,
-            tabs: [
-              Tab(icon: Icon(Icons.home), text: 'Home'),
-              Tab(icon: Icon(Icons.question_mark), text: 'Question'),
-              Tab(icon: Icon(Icons.people), text: 'People'),
-              Tab(icon: Icon(Icons.info), text: 'Info'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset('assets/images/TI.png'),
+              Image.asset('assets/images/TI2.jpg'),
+              Image.asset('assets/images/TI3.avif'),
             ],
           ),
         ),
-        body: TabBarView(children: [HomeScreen()]),
       ),
     );
   }
