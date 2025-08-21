10341ddcb7532a06df37af2b7ab103c051736816
Eduardo Huacac <eduardoahr2324@gmail.com>
2025-08-21 00:15:40 -0500

    Ejercicio 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:guia2/screens/home_screen.dart';
 
-void main() => runApp(const Myapp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,11 +22,27 @@ class PlantsBook extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4, 
+      length: 4,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
+          backgroundColor: Colors.amber.shade300,
+          title: Text(
+            'Medicina Natural',
+            style: TextStyle(color: const Color.fromARGB(255, 8, 8, 8)),
+          ),
+          bottom: TabBar(
+            indicatorColor: Colors.blue,
+            labelColor: Colors.white,
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.question_mark), text: 'Question'),
+              Tab(icon: Icon(Icons.people), text: 'People'),
+              Tab(icon: Icon(Icons.info), text: 'Info'),
+            ],
+          ),
         ),
-      ))
+        body: TabBarView(children: [HomeScreen()]),
+      ),
+    );
   }
 }
