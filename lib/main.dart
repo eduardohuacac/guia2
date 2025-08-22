84204ef0ac754fd27b0e99c0832527780427edec
Eduardo Huacac <eduardoahr2324@gmail.com>
2025-08-22 10:56:13 -0500

    ejemplo 5

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,36 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.star, color: Colors.green[500]),
-              Icon(Icons.star, color: Colors.green[500]),
-              Icon(Icons.star, color: Colors.green[500]),
-              const Icon(Icons.star, color: Colors.black),
-              const Icon(Icons.star, color: Colors.black),
-            ],
-          ),
-        ),
+      home: Scaffold(body: Center(child: _buildImageColumn())),
+    );
+  }
+
+  Widget _buildImageColumn() {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.black26),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [_buildImageRow(1), _buildImageRow(3)],
       ),
     );
   }
+
+  Widget _buildImageRow(int imageIndex) => Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      _buildDecoratedImage(imageIndex),
+      _buildDecoratedImage(imageIndex + 1),
+    ],
+  );
+
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+    child: Container(
+      margin: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        border: Border.all(width: 4, color: Colors.black38),
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+      ),
+      child: Image.asset('assets/images/TI2.jpg', fit: BoxFit.cover),
+    ),
+  );
 }
